88d3e05d57c07084a978a522c71a56b4e6b65295
Tan Nguyen <tan@sharkpunch.com>
2015-12-22 11:09:55 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+var Jasmine = require('jasmine')
+  , eslint = require('gulp-eslint')
+  , babel = require('gulp-babel');
+
+module.exports = function(gulp) {
+  gulp.task('lint', function() {
+    var paths = [
+      'src/**/*.js',
+      'tests/**/*.js'
+    ];
+    return gulp.src(paths)
+      .pipe(eslint())
+      .pipe(eslint.format())
+      .pipe(eslint.failOnError());
+  });
+
+  gulp.task('test', ['lint'], function(done) {
+    var jasmine = new Jasmine()
+      , config = {};
+
+    config.spec_files = [
+      'tests/**/*.spec.js'
+    ];
+    config.spec_dir = '.';
+    config.helpers = [
+      'node_modules/babel-register/lib/node.js'
+    ];
+
+    jasmine.loadConfig(config);
+    jasmine.configureDefaultReporter({
+      showColors: true,
+      forceExit: true
+    });
+
+    jasmine.onComplete(function() {
+      done();
+    });
+
+    jasmine.execute();
+  });
+
+  gulp.task('build', () => {
+    return gulp.src('src/**/*.js')
+      .pipe(babel({
+        presets: ['es2015']
+      }))
+      .pipe(gulp.dest('build'));
+  });
+
+  gulp.task('default', ['test', 'build']);
+
+  return gulp;
+}
